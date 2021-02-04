56c9d9c426b03f017cfed4e4fa77a41b873bbb97
Brian Thex <brian.thex@gmail.com>
2021-02-04 13:49:40 -0600

    project upload

---
+++ Assets/_Scripts/PlayerInput.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LP.ThirdPersonControllerNewInputSystemTut
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerInput : MonoBehaviour
+    {
+        [SerializeField] private InputAction movement;
+        [SerializeField] private InputAction turning;
+
+        private CharacterController controller = null;
+
+        public float speed = 7;
+        public float turnSpeed = 100;
+
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+
+        private void OnEnable()
+        {
+            movement.Enable();
+            turning.Enable();
+        }
+
+        private void OnDisable()
+        {
+            movement.Disable();
+            turning.Disable();
+        }
+
+        private void Update()
+        {
+            Walk();
+            Turn();
+        }
+
+        private void Walk()
+        {
+            float x = movement.ReadValue<Vector2>().x;
+            float z = movement.ReadValue<Vector2>().y;
+
+            Vector3 direction = transform.right * x + transform.forward * z;
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        private void Turn()
+        {
+            float y = turning.ReadValue<float>() * turnSpeed * Time.deltaTime;
+
+            transform.Rotate(Vector3.up * y);
+        }
+    }
+}
+
